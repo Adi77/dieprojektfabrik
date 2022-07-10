edacb6fd5a494bba648b1fbb35db9bf84f172b79
Adrian Felder <adrianfelder@gmx.ch>
2022-07-10 15:28:00 +0200

    first commit

---
+++ dieprojektfabrik/src/js/site.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+//import 'bootstrap';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import './../scss/style.scss';
+
+$(document).ready(function ($) {
+  console.log('hello world!');
+});
